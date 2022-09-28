da2d72b4555e95b3989812b90a85e92603e1caa2
Fabricio Louzada Colodette <fabricio.colodette2001@gmail.com>
2022-09-28 18:45:38 -0300

    Lendo pela webcam

--- main.py
+++ main.py
@@ -3,24 +3,49 @@ from pyzbar import pyzbar
 
 path = "barcode.png"
 
-img = cv2.imread(path)
+# img = cv2.imread(path)
 
+cap = cv2.VideoCapture(0)
 
-barcodes = pyzbar.decode(img)
-code = len(barcodes)
+while True:
 
-if (code != 0):
-    for barcode in barcodes:
-        (x, y, w, h) = barcode.rect
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        barcodeData = barcode.data.decode("utf-8")
-        barcodeType = barcode.type
-        text = "{} ({})".format(barcodeData, barcodeType)
-        cv2.putText(img, text, (x, y - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        print("Type: {}".format(barcodeType))
-        print("Data: {}".format(barcodeData))
+    ret, img = cap.read()
 
-cv2.imshow("Image", img)
-cv2.waitKey(0)
+    barcodes = pyzbar.decode(img)
+    code = len(barcodes)
+
+    if (code != 0):
+        for barcode in barcodes:
+            (x, y, w, h) = barcode.rect
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            barcodeData = barcode.data.decode("utf-8")
+            barcodeType = barcode.type
+            text = "{} ({})".format(barcodeData, barcodeType)
+            cv2.putText(img, text, (x, y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            print("Type: {}".format(barcodeType))
+            print("Data: {}".format(barcodeData))
+
+    cv2.imshow("img", img)
+    if cv2.waitKey(1) == 27:
+        break
+
 cv2.destroyAllWindows()
+# barcodes = pyzbar.decode(img)
+# code = len(barcodes)
+
+# if (code != 0):
+#     for barcode in barcodes:
+#         (x, y, w, h) = barcode.rect
+#         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+#         barcodeData = barcode.data.decode("utf-8")
+#         barcodeType = barcode.type
+#         text = "{} ({})".format(barcodeData, barcodeType)
+#         cv2.putText(img, text, (x, y - 10),
+#                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+#         print("Type: {}".format(barcodeType))
+#         print("Data: {}".format(barcodeData))
+
+# cv2.imshow("Image", img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
